12b352bae82aea52a57c776b14113a27a6d3ecec
VReezus <mrsizer22613@gmail.com>
2023-01-20 17:20:30 +0600

    norm

--- review/models.py
+++ review/models.py
@@ -8,12 +8,10 @@ class Product(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField()
 
-
 class Like(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
-
 
 class Rating(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -21,13 +19,11 @@ class Rating(models.Model):
     rating = models.IntegerField()
     created_at = models.DateTimeField(auto_now_add=True)
 
-
 class Comment(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     text = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
-
 
 class Reply(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
