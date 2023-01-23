0fa7e98e409fb0fcf3b1178537ca74945056c0fb
Kalys <jsvsjsvshsksb@gmail.com>
2023-01-23 15:52:08 +0600

    balance

--- review/admin.py
+++ review/admin.py
@@ -2,8 +2,8 @@ from django.contrib import admin
 
 from .models import *
 
-admin.site.register(Comment)
-admin.site.register(LikeComment)# ?
-admin.site.register(LikeProduct)# ?
-admin.site.register(Rating)
-admin.site.register(Favourite)
+# admin.site.register(Comment)
+# admin.site.register(LikeComment)# ?
+# admin.site.register(LikeProduct)# ?
+# admin.site.register(Rating)
+# admin.site.register(Favourite)
