49c56f978b9ca644d5250d61cefe150132b899db
Kalys <jsvsjsvshsksb@gmail.com>
2023-01-24 11:23:30 +0600

    account

--- account/urls.py
+++ account/urls.py
@@ -18,7 +18,7 @@ list -----------> users/ GET
 retrieve -------> users/id/ GET
 update ---------> users/id/ PUT
 partial-update -> users/id/ PATCH
-destroy --------> users/id/ DELETE
+destroy --------> users/id/ DELETE 
 '''
 
 
@@ -26,28 +26,30 @@ destroy --------> users/id/ DELETE
 
 
 urlpatterns = [
-    path('', include(router.urls)),
+    path('', include(router.urls)), # админка по упралению юзерами (16-21 строки)
 
-    path('token/', TokenObtainPairView.as_view()),
+    path('token/', TokenObtainPairView.as_view()), 
     path('token/refresh/', TokenRefreshView.as_view()),
     path('logout/', LogoutView.as_view()),
     
-    path('user/', UserAPIView.as_view()),
+    path('user/', UserAPIView.as_view()), # управление юзера юзером (get, patch, delete)
     
-    path('get_code/', get_code),
+    path('get_code/', get_code), # получение кода приглашения
+
+    path('register/<str:code>/', RegisterAPIView.as_view()), # регистрация (только через бота) (email, password, password_confirm)
+    path('activate/<str:activation_code>/', activate_view), # подтверждение (ссылка)
+
 
 # urls of Bot
-    path('register/<str:code>/', RegisterAPIView.as_view()),
     path('get_code_bot/', get_code_link),
     path('check_code/', check_code),
 #
     
-    path('activate/<str:activation_code>/', activate_view),
 
-    path('update_balance/', balance_update),
-    path('payment/<str:activation_code>/<int:amount>/', payment_confirm),
+    path('update_balance/', balance_update), # пополнение баланса (amount)
+    path('payment/<str:activation_code>/<int:amount>/', payment_confirm), # подтверждение оплаты (почта)
    
-    path('change_password/', ChangePasswordView.as_view()),
-    path('forgot_password/', password_recover),
-    path('forgot_password_confirm/<str:activation_code>/<str:new_password>/', password_confirm)
+    path('change_password/', ChangePasswordView.as_view()), # изменение пароля (email, old_password, new_password)
+    path('forgot_password/', password_recover), # сбросить пароль
+    path('forgot_password_confirm/<str:activation_code>/<str:new_password>/', password_confirm) # подтверждение сброса пароля (на почту выслан новый пароль)
 ]
--- test2.py
+++
@@ -1,7 +0,0 @@
-from account.models import Code
-
-answer = input('Введите код: ')
-if Code.objects.filter(code=answer).exists():
-    print('yes')
-else:
-    print('no')
